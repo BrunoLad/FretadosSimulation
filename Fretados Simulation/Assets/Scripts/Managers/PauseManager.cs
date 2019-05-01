234c6ef22045cb816ba38fae55802a76763d0316
Bruno Ladeia <bruno13dm@outlook.com>
2019-05-01 18:24:09 -0300

    Created Pause Screen

--- Fretados Simulation/Assets/Scripts/Managers/PauseManager.cs
+++ Fretados Simulation/Assets/Scripts/Managers/PauseManager.cs
@@ -2,58 +2,103 @@
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
-public class PauseManager : MonoBehaviour {
-	
-	public AudioMixerSnapshot paused;
-	public AudioMixerSnapshot unpaused;
-	
-	Canvas canvas;
-	
-	void Start()
-	{
-		canvas = GetComponent<Canvas>();
-	}
-	
-	void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.Escape))
-		{
-			canvas.enabled = !canvas.enabled;
-			Pause();
-		}
-	}
-	
-	public void Pause()
-	{
-		Time.timeScale = Time.timeScale == 0 ? 1 : 0;
-		Lowpass ();
-		
-	}
-	
-	void Lowpass()
-	{
-		if (Time.timeScale == 0)
-		{
-			paused.TransitionTo(.01f);
-		}
-		
-		else
-			
-		{
-			unpaused.TransitionTo(.01f);
-		}
-	}
-	
-	public void Quit()
-	{
-		#if UNITY_EDITOR 
-		EditorApplication.isPlaying = false;
-		#else 
+public class PauseManager : MonoBehaviour
+{
+
+    public AudioMixerSnapshot paused;               // snapshot para áudio enquanto o jogo estiver pausado
+    public AudioMixerSnapshot unpaused;             // snapshot para áudio enquanto o jogo não estiver pausado
+
+    GameObject[] pauseObjects;                      // Referência ao menu de pausa do jogo
+
+    void Start()
+    {
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        HidePaused();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    // Controla a pausa da cena e cria a referência para controlar via botão
+    public void Pause()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            ShowPaused();
+        }
+        else
+        {
+            Time.timeScale = 1;
+            HidePaused();
+        }
+
+        Lowpass();
+    }
+
+    void Lowpass()
+    {
+        if (Time.timeScale == 0)
+        {
+            paused.TransitionTo(.01f);
+        }
+
+        else
+
+        {
+            unpaused.TransitionTo(.01f);
+        }
+    }
+
+    //Esconde o menu de pausa
+    private void HidePaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    // Mostra o menu de pausa
+    private void ShowPaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    // Reinicia o jogo
+    public void Reload()
+    {
+        Pause();
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    //Carrega a cena escolhida
+    public void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    //Encerra o jogo
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+        #else
 		Application.Quit();
-		#endif
-	}
+        #endif
+    }
 }
